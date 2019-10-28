f666e13a876ad18f26c18853627ea7653d784768
bodaghialib4 <bodaghialib4@gmail.com>
2019-10-28 23:30:57 +0330

    add test ReactMethod to test jcifs library (test existence of a file in smb server) usage: RNSmb.test(String workGroup, String ip, String username, String password, String sharedFolder,String fileName)

--- android/src/main/java/com/reactlibrarysmbbodaghi/RNSmbModule.java
+++ android/src/main/java/com/reactlibrarysmbbodaghi/RNSmbModule.java
@@ -6,6 +6,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import android.widget.Toast;
+import android.text.TextUtils;
+
+
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileOutputStream;
 
 public class RNSmbModule extends ReactContextBaseJavaModule {
 
@@ -25,7 +31,30 @@ public class RNSmbModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void show(String text) {
     //ReactApplicationContext context = getReactApplicationContext();
-    Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_LONG).show();
+    Toast.makeText(getReactApplicationContext(), text, Toast.LENGTH_SHORT).show();
+  }
+
+  @ReactMethod
+  public void test(String workGroup, String ip, String username, String password, String sharedFolder,String fileName) {
+    try {
+      NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(workGroup, username, password);
+
+      SmbFile sFile;
+      String url = "smb://" + ip + "/" + sharedFolder + "/" + fileName;
+      if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
+        sFile = new SmbFile(url, auth);
+      } else {
+        sFile = new SmbFile(url);
+      }
+
+      String message = "can read" + " : " + String.valueOf(sFile.canRead());
+
+      Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    } catch (Exception e) {
+      // Output the stack trace.
+      e.printStackTrace();
+      String message = " error in testing SMB";
+    }
   }
 
 }
