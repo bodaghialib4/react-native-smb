cfc61b91eec7190abc43a979cb38dfad3d7bdfa3
bodaghialib4 <bodaghialib4@gmail.com>
2019-11-18 11:22:42 +0330

    refactor native delete function

--- android/src/main/java/com/reactlibrarysmbbodaghi/RNSmbModule.java
+++ android/src/main/java/com/reactlibrarysmbbodaghi/RNSmbModule.java
@@ -1148,51 +1148,57 @@ public class RNSmbModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void delete(
-          @Nullable final String targetPath
-          ) {
-      extraThreadPool.execute(new Runnable() {
-        @Override
-        public void run() {
-          WritableMap statusParams = Arguments.createMap();
-          statusParams.putString("path", targetPath + "");
-          try {
-            String fullTargetPath = "/";
-            if (targetPath != null && !TextUtils.isEmpty(targetPath)) {
-              fullTargetPath = "/" + targetPath + fullTargetPath;
-            }
-            SmbFile targetSmbFile;
-            if (authentication != null) {
-              targetSmbFile = new SmbFile(serverURL + fullTargetPath, authentication);
-            } else {
-              targetSmbFile = new SmbFile(serverURL + fullTargetPath);
-            }
-
-            if (!targetSmbFile.exists()) {
-              statusParams.putBoolean("success", false);
-              statusParams.putString("message", " can not find file or directory to delete [sourcePath]!!");
-            } else if (!targetSmbFile.canWrite()) {
-              statusParams.putBoolean("success", false);
-              statusParams.putString("message", " no permission to delete file or directory [sourcePath]!!");
-            } else {
-              targetSmbFile.delete();
-                if (!targetSmbFile.exists()) {
-                  statusParams.putBoolean("success", true);
-                  statusParams.putString("message", "directory or file successfully deleted[" + targetSmbFile.getPath() + "]");
-                } else {
-                  statusParams.putBoolean("success", false);
-                  statusParams.putString("message", "directory or file not deleted in server [" + targetSmbFile.getPath() + "]!!!!");
-                }
-
-            }
-          } catch (Exception e) {
-            // Output the stack trace.
-            e.printStackTrace();
-            statusParams.putBoolean("success", false);
-            statusParams.putString("message", "delete exception error: " + e.getMessage());
+          final String clientId,
+          @Nullable final String targetPath,
+          final Callback callback
+  ) {
+    extraThreadPool.execute(new Runnable() {
+      @Override
+      public void run() {
+        WritableMap statusParams = Arguments.createMap();
+        statusParams.putString("name", "delete");
+        statusParams.putString("clientId", clientId);
+        statusParams.putString("targetPath", targetPath + "");
+        try {
+          String fullTargetPath = "/";
+          if (targetPath != null && !TextUtils.isEmpty(targetPath)) {
+            fullTargetPath = fullTargetPath + targetPath;
           }
-          sendEvent(reactContext, "SMBDeleteResult", statusParams);
+          SmbFile targetSmbFile;
+          NtlmPasswordAuthentication authentication = authenticationPool.get(clientId);
+          String serverURL = serverURLPool.get(clientId);
+          if (authentication != null) {
+            targetSmbFile = new SmbFile(serverURL + fullTargetPath, authentication);
+          } else {
+            targetSmbFile = new SmbFile(serverURL + fullTargetPath);
+          }
+
+          if (!targetSmbFile.exists()) {
+            statusParams.putBoolean("success", false);
+            statusParams.putString("errorCode", "1111");
+            statusParams.putString("message", " can not find file or directory to delete [" + targetSmbFile.getPath() + "]!!");
+          } else if (!targetSmbFile.canWrite()) {
+            statusParams.putBoolean("success", false);
+            statusParams.putString("errorCode", "1111");
+            statusParams.putString("message", " no permission to delete file or directory [" + targetSmbFile.getPath() + "]!!");
+          } else {
+            targetSmbFile.delete();
+
+            statusParams.putBoolean("success", true);
+            statusParams.putString("errorCode", "0000");
+            statusParams.putString("message", "directory or file successfully deleted[" + targetSmbFile.getPath() + "]");
+          }
+        } catch (Exception e) {
+          // Output the stack trace.
+          e.printStackTrace();
+          statusParams.putBoolean("success", false);
+          statusParams.putString("errorCode", "0101");
+          statusParams.putString("message", "delete exception error: " + e.getMessage());
         }
-      });
+        callback.invoke(statusParams);
+      }
+    });
+  }
 
   @ReactMethod
   public void disconnect(
